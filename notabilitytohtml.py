8bf9e3c581e2950ddebfcc188b11fe7546f4a13b
tacocats <john.moriarty@student.fairfield.edu>
2018-10-16 18:50:25 -0400

    Work on reading file

--- notabilitytohtml.py
+++ notabilitytohtml.py
@@ -1,11 +1,30 @@
 """ Notability to HTML converter """
 
 import sys
+import plistlib
+import zipfile
+import os
 
 class nreader():
     """ Read and parse Notability format """
-    pass
 
+    def __init__(self):
+        pass
+
+    def readFile(self, file_name, directory=os.getcwd()):
+        self.file_name = file_name
+        self.directory = directory
+
+        self.unzipFiles()
+
+    def unzipFiles(self):
+        """ Unzip specified file to specified location """
+        with zipfile.ZipFile(self.file_name, 'r') as fn:
+            fn.extractall(self.directory)
+    
+    def parsePlst(self):
+        pass
+    
 
 def displayHelp():
     print ("Arguments:")
@@ -21,6 +40,11 @@ def main():
     for arg in sys.argv[1:]:
         print (arg)
 
+def test():
+    r = nreader()
+    r.readFile("testfile.note")
+
 
 if __name__ == "__main__":
-    main()
+    #main()
+    test()
